50352445c12fd4be52a20a445043a30525627214
Priyanshu Praneet <148612946+PriyanshuPraneet@users.noreply.github.com>
2025-12-09 15:30:34 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (70.36%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int left = m-1;
-        int right = 0;
-        while(left>=0 && right<n){
-            if(nums1[left] > nums2[right]){
-                swap(nums1[left],nums2[right]);
-                right++;
-                left--;
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k] = nums1[i];
+                k--;
+                i--;
             }
             else{
-                break;
+                nums1[k] = nums2[j];
+                k--;
+                j--;
             }
         }
-        sort(nums1.begin(),nums1.end()-n);
-        sort(nums2.begin(),nums2.end());
-
-        int idx = 0;
-        for(int i=m; i<m+n; i++){
-            nums1[i] = nums2[idx];
-            idx++;
+        while(i>=0){
+            nums1[k] = nums1[i];
+            k--;
+            i--;
+        }
+        while(j>=0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 };
