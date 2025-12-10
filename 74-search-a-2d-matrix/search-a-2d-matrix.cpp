50e0499a268485940eadb31c2f0fef1d6a2507e2
Priyanshu Praneet <148612946+PriyanshuPraneet@users.noreply.github.com>
2025-12-10 15:18:47 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (41.48%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,14 +4,18 @@ public:
         int row = matrix.size();
         int col = matrix[0].size();
         int beg = 0;
-        int end = (row * col) - 1;
+        int end = row*col-1;
         while(beg<=end){
             int mid = (beg+end)/2;
             int rowIdx = mid/col;
             int colIdx = mid%col;
-            if(matrix[rowIdx][colIdx] == target) return true;
-            else if(matrix[rowIdx][colIdx] > target) end = mid-1;
-            else beg = mid+1; 
+            if(matrix[rowIdx][colIdx]==target){
+                return true;
+            }
+            else if(matrix[rowIdx][colIdx]<target){
+                beg = mid+1;
+            }
+            else end = mid-1;
         }
         return false;
     }
