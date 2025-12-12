dea70d29c93a4e7d2757912029efeb633fb179e0
Priyanshu Praneet <148612946+PriyanshuPraneet@users.noreply.github.com>
2025-12-12 16:32:31 +0530

    Time: 529 ms (30.63%) | Memory: 243.4 MB (28.35%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.cpp
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void merge(vector<int>& nums, int low, int mid, int high){
+        vector<int>temp;
         int left = low;
         int right = mid+1;
-        vector<int> temp;
         while(left<=mid && right<=high){
-            if(nums[left]<nums[right]){
+            if(nums[left]<=nums[right]){
                 temp.push_back(nums[left]);
                 left++;
             }
@@ -26,29 +26,27 @@ public:
             nums[i] = temp[i-low];
         }
     }
-    int getCount(vector<int>& nums, int low, int mid, int high){
+    int countPairs(vector<int>& nums, int low, int mid, int high){
         int count = 0;
         int right = mid+1;
         for(int i=low; i<=mid; i++){
-            while(right<=high && nums[i] > 2 * (long)nums[right])right++;
-            count+=(right-(mid+1));
+            while(right<=high && nums[i]>(long long)2*nums[right]) right++;
+            count += (right-(mid+1));
         }
         return count;
     }
-    int mergeSort(vector<int>& nums,int low, int high){
+    int mergeSort(vector<int>& nums, int low, int high){
         int count = 0;
-        if(low>=high){
-            return count;
-        }
+        if(low>=high) return count;
         int mid = (low+high)/2;
-        count += mergeSort(nums,low,mid);
-        count += mergeSort(nums,mid+1,high);
-        count += getCount(nums,low,mid,high);
+        count+=mergeSort(nums,low,mid);
+        count+=mergeSort(nums,mid+1,high);
+        count+=countPairs(nums,low,mid,high);
         merge(nums,low,mid,high);
         return count;
     }
     int reversePairs(vector<int>& nums) {
-        int high = nums.size()-1;
-        return mergeSort(nums,0,high);
+        int n=nums.size();
+        return mergeSort(nums,0,n-1);
     }
 };
