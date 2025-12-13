be1626b552d072d364fc5e14db997d82e7893e7f
Priyanshu Praneet <148612946+PriyanshuPraneet@users.noreply.github.com>
2025-12-13 15:17:14 +0530

    Time: 16 ms (78.51%) | Memory: 17.3 MB (82.87%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -4,22 +4,22 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         for(int i=0; i<nums.size(); i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
+            if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1; j<nums.size(); j++){
                 if(j>i+1 && nums[j]==nums[j-1]) continue;
-                int k = j+1;
-                int l = nums.size()-1;
-                while(k<l){
-                    long long sum = (long long) nums[i]+nums[j]+nums[k]+nums[l];
-                    if(sum<target) k++;
-                    else if(sum>target) l--;
-                    else{
-                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
-                        k++;
-                        l--;
-                        while(k<l && nums[k] == nums[k-1]) k++;
-                        while(k<l && nums[l] == nums[l+1]) l--;
+                int low = j+1;
+                int high = nums.size()-1;
+                while(low<high){
+                    long long sum = (long long)nums[i]+nums[j]+nums[low]+nums[high];
+                    if(target==sum){
+                        ans.push_back({nums[i],nums[j],nums[low],nums[high]});
+                        low++;
+                        high--;
+                        while(low<high && nums[low]==nums[low-1]) low++;
+                        while(low<high && nums[high]==nums[high+1]) high--;
                     }
+                    else if(sum>target) high--;
+                    else low++;
                 }
             }
         }
