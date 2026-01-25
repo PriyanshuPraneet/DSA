e433e909370116e971725b1116bdd8174ab0c3f9
Priyanshu Praneet <148612946+PriyanshuPraneet@users.noreply.github.com>
2026-01-25 18:09:17 +0530

    Time: 1 ms (46.37%) | Memory: 10.3 MB (84.45%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    void getSubsetsWithDup(vector<int>& nums, vector<vector<int>>& ans, vector<int>& temp,
-    int idx){
-        if(idx==nums.size()){
-            ans.push_back(temp);
-            return;
+    void getSubset(vector<int>& nums, vector<int>& temp, vector<vector<int>>& ans, int idx)
+    {
+        ans.push_back(temp);
+        for(int i=idx; i<nums.size(); i++){
+            if(i!=idx && nums[i]==nums[i-1]) continue;
+            temp.push_back(nums[i]);
+            getSubset(nums,temp,ans,i+1);
+            temp.pop_back();
         }
-        temp.push_back(nums[idx]);
-        getSubsetsWithDup(nums,ans,temp,idx+1);
-        temp.pop_back();
-        while(idx<(nums.size()-1) && nums[idx]==nums[idx+1]) idx++;
-        getSubsetsWithDup(nums,ans,temp,idx+1);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        vector<vector<int>> ans;
         vector<int> temp;
-        getSubsetsWithDup(nums,ans,temp,0);
-        sort(ans.begin(),ans.end());
+        vector<vector<int>> ans;
+        getSubset(nums,temp,ans,0);
         return ans;
     }
 };
