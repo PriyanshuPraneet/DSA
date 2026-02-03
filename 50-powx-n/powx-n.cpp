c05810111dc4abea0c4fcb62156e76b2417c9e21
Priyanshu Praneet <148612946+PriyanshuPraneet@users.noreply.github.com>
2026-02-03 22:26:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (13.89%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-    void getAns(double x, long long n, double& ans){
-        if(n<=0){
-            ans = 1;
-            return;
-        }
-        if(n%2==0){
-            getAns(x,n/2,ans);
-            ans = ans * ans;
-        }
-        else{
-            getAns(x,n-1,ans);
-            ans = x * ans;
-        }
+    double getAns(double x, long long n){
+        if(n<=0) return 1;
+        double ans = getAns(x,n/2);
+        ans = ans * ans;
+        if(n%2==1) ans = x * ans;
+        return ans;
     }
-    double myPow(double x, int N) {
-        long long n = N;
-        double ans = 1;
-        if(n<0){
-            n = n * -1;
-            getAns(x,n,ans);
-            return 1/ans;
-        }
-        getAns(x,n,ans);
+    double myPow(double x, int n) {
+        long long N = n;
+        if(N<0) N = N * -1;
+        double ans = getAns(x,N);
+        if(n<0) return 1/ans;
         return ans;
     }
 };
